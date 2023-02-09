1bbaf6b86ec8c07ee0d00cb1fe92280d95cac0d7
Dreb <sergbutenko5243@gmail.com>
2023-02-09 19:56:25 +0300

    second programm

--- web.py
+++ web.py
@@ -4,6 +4,7 @@ import sys
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+from PyQt5 import QtCore
 
 SCREEN_SIZE = [600, 450]
 lst = []
@@ -12,37 +13,33 @@ lst = []
 class Programm(QWidget):
     def __init__(self):
         super().__init__()
+        self.delta = '0.005'
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        # map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-        toponym_to_find = " ".join(sys.argv[1:])
+        try:
+            self.toponym_lattitude, self.toponym_longitude = lst[0], lst[1]
+            map_params = {
+                "ll": ",".join([self.toponym_longitude, self.toponym_lattitude]),
+                "spn": ",".join([self.delta, self.delta]),
+                "l": "map"
+            }
 
-        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+            map_api_server = "http://static-maps.yandex.ru/1.x/"
+            response = requests.get(map_api_server, params=map_params)
 
-        toponym_lattitude, toponym_longitude = lst[0], lst[1]
-        delta = '0.005'
-        map_params = {
-            "ll": ",".join([toponym_longitude, toponym_lattitude]),
-            "spn": ",".join([delta, delta]),
-            "l": "map"
-        }
+            if not response:
+                print("Ошибка выполнения запроса:")
+                print("Http статус:", response.status_code, "(", response.reason, ")")
+                sys.exit(1)
 
-        map_api_server = "http://static-maps.yandex.ru/1.x/"
-        response = requests.get(map_api_server, params=map_params)
-        # response = requests.get(map_request)
-
-        if not response:
-            print("Ошибка выполнения запроса:")
-            # print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
-
-        # Запишем полученное изображение в файл.
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
+            # Запишем полученное изображение в файл.
+            self.map_file = "map.png"
+            with open(self.map_file, "wb") as file:
+                file.write(response.content)
+        except Exception as e:
+            print(e)
 
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
@@ -59,12 +56,31 @@ class Programm(QWidget):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
+    def keyPressEvent(self, event):
+        k = 0.005
+        if event.key() == QtCore.Qt.Key_PageDown:
+            if float(self.delta) < k * 100:
+                self.delta = str(float(self.delta) + k)
+                self.getImage()
+                self.image.setPixmap(QPixmap('map.png'))
+                self.image.show()
+        if event.key() == QtCore.Qt.Key_PageUp:
+            if float(self.delta) > k:
+                self.delta = str(float(self.delta) - k)
+                self.getImage()
+                self.image.setPixmap(QPixmap('map.png'))
+                self.image.show()                   
+                
+        print(self.delta)
+        event.accept()
+
+
 
 class Request(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-    
+
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.name_input = QLineEdit(self)
@@ -75,7 +91,7 @@ class Request(QWidget):
         self.btn.resize(self.btn.sizeHint())
         self.btn.move(100, 150)
         self.btn.clicked.connect(self.run)
-    
+
     def run(self):
         global lst
         lst += self.name_input.text().split(', ')
